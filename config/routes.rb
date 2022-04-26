3b4984ae5eab6c0209c1ea2e640c584128b9cdd2
Rahul <rahul.k@simformsolutions.com>
2022-04-26 17:44:12 +0530

    Added controller and pg gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "static_page#landing_page"
+  get "privacy_policy", to: "static_page#privacy_policy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
